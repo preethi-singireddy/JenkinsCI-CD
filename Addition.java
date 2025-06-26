0c92023750f9ba6192ed8dc4fe56dd59ccb14660
Spoorthy Reddy <mamillapallyspoorthy@gmail.com>
2025-06-26 10:04:23 +0530

    Added Addition.java program

---
+++ Addition.java
@@ -0,0 +1,11 @@
+public class Addition {
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        int sum = add(15, 25);
+        System.out.println("Sum of 15 and 25 is: " + sum);
+    }
+}
